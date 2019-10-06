4d963da08646f548efc18492eae65fe9fe055322
Philippe Esling <esling@PRO-2903-esling.local>
2019-10-06 22:59:49 +0200

    Add signal library structure

---
+++ signal/generators.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+import torch
+import torch.nn as nn
+    
+class Generator(nn.Module):
+    
+    def __init__(self):
+        super(Filter, self).__init__()
+        self.apply(self.init_parameters)
+    
+    def init_parameters(self, m):
+        pass
+
+    def forward(self, x):
+        pass
