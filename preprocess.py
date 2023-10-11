0f659261147925ba281826e1b38f6c6619d9eae8
maxardito <maxwellardito@gmail.com>
2023-10-11 14:05:26 -0400

    Centroid preprocessing

--- preprocess.py
+++ preprocess.py
@@ -1,14 +1,13 @@
 import yaml
 import pathlib
 import librosa as li
-from ddsp.core import extract_loudness, extract_pitch
+from ddsp.core import extract_loudness, extract_centroid
 from effortless_config import Config
 import numpy as np
 from tqdm import tqdm
 import numpy as np
 from os import makedirs, path
 import torch
-from scipy.io import wavfile
 
 
 def get_files(data_location, extension, **kwargs):
@@ -16,21 +15,21 @@ def get_files(data_location, extension, **kwargs):
 
 
 def preprocess(f, sampling_rate, block_size, signal_length, oneshot, **kwargs):
-    x, sr = li.load(f)
+    x, _ = li.load(f)
     N = (signal_length - len(x) % signal_length) % signal_length
     x = np.pad(x, (0, N))
 
     if oneshot:
         x = x[..., :signal_length]
 
-    pitch = extract_pitch(x, sampling_rate, block_size)
-    loudness = extract_loudness(x, sampling_rate, block_size)
+    centroid = extract_centroid(x, sampling_rate, block_size)
+    loudness = extract_loudness(x, block_size)
 
     x = x.reshape(-1, signal_length)
-    pitch = pitch.reshape(x.shape[0], -1)
+    centroid = centroid.reshape(x.shape[0], -1)
     loudness = loudness.reshape(x.shape[0], -1)
 
-    return x, pitch, loudness
+    return x, centroid, loudness
 
 
 class Dataset(torch.utils.data.Dataset):
@@ -38,7 +37,7 @@ class Dataset(torch.utils.data.Dataset):
     def __init__(self, out_dir):
         super().__init__()
         self.signals = np.load(path.join(out_dir, "signals.npy"))
-        self.pitchs = np.load(path.join(out_dir, "pitchs.npy"))
+        self.centroids = np.load(path.join(out_dir, "centroids.npy"))
         self.loudness = np.load(path.join(out_dir, "loudness.npy"))
 
     def __len__(self):
@@ -46,9 +45,9 @@ class Dataset(torch.utils.data.Dataset):
 
     def __getitem__(self, idx):
         s = torch.from_numpy(self.signals[idx])
-        p = torch.from_numpy(self.pitchs[idx])
+        c = torch.from_numpy(self.centroids[idx])
         l = torch.from_numpy(self.loudness[idx])
-        return s, p, l
+        return s, c, l
 
 
 def main():
@@ -63,25 +62,26 @@ def main():
     pb = tqdm(files)
 
     signals = []
-    pitchs = []
+
+    centroids = []
     loudness = []
 
     for f in pb:
         pb.set_description(str(f))
-        x, p, l = preprocess(f, **config["preprocess"])
+        x, c, l = preprocess(f, **config["preprocess"])
         signals.append(x)
-        pitchs.append(p)
+        centroids.append(c)
         loudness.append(l)
 
     signals = np.concatenate(signals, 0).astype(np.float32)
-    pitchs = np.concatenate(pitchs, 0).astype(np.float32)
+    centroids = np.concatenate(centroids, 0).astype(np.float32)
     loudness = np.concatenate(loudness, 0).astype(np.float32)
 
     out_dir = config["preprocess"]["out_dir"]
     makedirs(out_dir, exist_ok=True)
 
     np.save(path.join(out_dir, "signals.npy"), signals)
-    np.save(path.join(out_dir, "pitchs.npy"), pitchs)
+    np.save(path.join(out_dir, "centroids.npy"), centroids)
     np.save(path.join(out_dir, "loudness.npy"), loudness)
 
 
